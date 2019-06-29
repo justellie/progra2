cf9c47aadfe5adae3bbe22450f15dd90be1aea88
Miranda <elioruiz.en@gmail.com>
2019-06-29 11:11:35 -0400

    LECTURA DE DATOS YEIgit add .!

--- guerra.cpp
+++ guerra.cpp
@@ -2,6 +2,39 @@
 #include "Nodo.h"
 #include "Listas.h"
 #include "Cola.h"
+#include <fstream>
+
 
 
 using namespace std;
+
+int main()
+{
+    Cola<int> uno, dos;
+    //ifstream myfile;
+
+   
+
+    int a , b,i;
+    i=0;
+    cin>> a;
+    
+    while(i<26)
+    {
+        cin>>b;
+        uno.encolar(b);
+        i+=1;
+    }
+    i=0;
+    while(i<26)
+    {
+        cin>>b;
+        dos.encolar(b);
+        i+=1;
+    }
+    uno.print();
+    dos.print();
+
+    
+
+}
